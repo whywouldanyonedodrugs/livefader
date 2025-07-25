ad601720b1d5935244d62116475c19fe5fff22de
whywouldanyonedodrugs <rimantas.murza@gmail.com>
2025-07-25 14:58:18 +0300

    fix: update import statement for pydantic to include Extra

--- live/live_trader.py
+++ live/live_trader.py
@@ -34,7 +34,7 @@ from .database import DB
 from .telegram import TelegramBot
 
 
-from pydantic import Field, ValidationError
+from pydantic import Field, Extra
 from pydantic_settings import BaseSettings
 
 # ---------------------------  YOUR MODULES  ------------------------------
