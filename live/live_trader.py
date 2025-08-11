11a4b9497b91d7153a0e3933295104ef51b06103
whywouldanyonedodrugs <rimantas.murza@gmail.com>
2025-08-11 13:23:48 +0300

    sadasd

--- live/live_trader.py
+++ live/live_trader.py
@@ -22,10 +22,6 @@ from collections import defaultdict
 from typing import Any, Dict, List, Optional, Tuple
 import subprocess
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if PROJECT_ROOT not in sys.path:
-    sys.path.insert(0, PROJECT_ROOT)
-
 import aiohttp
 import asyncpg
 import ccxt.async_support as ccxt
